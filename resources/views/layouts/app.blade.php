00c05fc36a90a06a543f6d289412f7fdb9dfd63d
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-06-09 00:26:01 +0700

    Remove unused css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ asset('js/corporate-ui-dashboard.min.js') }}?v=1.0.0"></script>
+  <script src="{{ asset('js/corporate-ui-dashboard.js') }}?v=1.0.0"></script>
 </body>
 
 </html>
