f50681420825091606b290d270c6f373e14bfbba
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-06-09 00:10:24 +0700

    Fix sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -8,7 +8,7 @@
     <div class="collapse navbar-collapse px-4  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link  " href="../pages/dashboard.html">
+          <a class="nav-link  " href="{{ route('dashboard') }}">
             <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
               <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>Dashboard</title>
@@ -25,7 +25,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  active" href="../pages/tables.html">
+          <a class="nav-link active" href="{{ route('medicines.index') }}">
             <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
               <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>table</title>
@@ -43,7 +43,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="../pages/wallet.html">
+          <a class="nav-link  " href="{{ route('sales.index') }}">
             <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
               <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>wallet</title>
@@ -59,34 +59,34 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="../pages/wallet.html">
-            <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 58 58">
-                    <path d="M24 4c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 6c3.31 0 6 2.69 6 6 0 3.32-2.69 6-6 6s-6-2.68-6-6c0-3.31 2.69-6 6-6zm0 28.4c-5.01 0-9.41-2.56-12-6.44.05-3.97 8.01-6.16 12-6.16s11.94 2.19 12 6.16c-2.59 3.88-6.99 6.44-12 6.44z" fill="#ffffff"/>
-                    <path d="M0 0h48v48h-48z" fill="none"/>
-                </svg>
+          <a class="nav-link  " href="{{ route('account.index') }}">
+            <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">      
+              <svg width="24" height="24" viewBox="-5 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <title>account</title>
+                <path d="M24 4c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 6c3.31 0 6 2.69 6 6 0 3.32-2.69 6-6 6s-6-2.68-6-6c0-3.31 2.69-6 6-6zm0 28.4c-5.01 0-9.41-2.56-12-6.44.05-3.97 8.01-6.16 12-6.16s11.94 2.19 12 6.16c-2.59 3.88-6.99 6.44-12 6.44z" fill="#ffffff"/>
+                <path d="M0 0h48v48h-48z" fill="none"/>
+              </svg>
             </div>
             <span class="nav-link-text ms-1">Account</span>
           </a>
         </li>
-    <li class="nav-item">
-        <a class="nav-link" href="../pages/rtl.html">
-            <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
-                <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <title>rtl</title>
-                    <g id="rtl" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <g id="menu-alt-3" transform="translate(12.000000, 14.000000)" fill="#FFFFFF">
-                            <path class="color-foreground" d="M0,1.71428571 C0,0.76752 0.76752,0 1.71428571,0 L22.2857143,0 C23.2325143,0 24,0.76752 24,1.71428571 C24,2.66105143 23.2325143,3.42857143 22.2857143,3.42857143 L1.71428571,3.42857143 C0.76752,3.42857143 0,2.66105143 0,1.71428571 Z" id="Path"></path>
-                            <path class="color-background" d="M0,10.2857143 C0,9.33894857 0.76752,8.57142857 1.71428571,8.57142857 L22.2857143,8.57142857 C23.2325143,8.57142857 24,9.33894857 24,10.2857143 C24,11.2325143 23.2325143,12 22.2857143,12 L1.71428571,12 C0.76752,12 0,11.2325143 0,10.2857143 Z" id="Path"></path>
-                            <path class="color-background" d="M10.2857143,18.8571429 C10.2857143,17.9103429 11.0532343,17.1428571 12,17.1428571 L22.2857143,17.1428571 C23.2325143,17.1428571 24,17.9103429 24,18.8571429 C24,19.8039429 23.2325143,20.5714286 22.2857143,20.5714286 L12,20.5714286 C11.0532343,20.5714286 10.2857143,19.8039429 10.2857143,18.8571429 Z" id="Path"></path>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}">
+                <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
+                    <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <title>rtl</title>
+                        <g id="rtl" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g id="menu-alt-3" transform="translate(12.000000, 14.000000)" fill="#FFFFFF">
+                                <path class="color-foreground" d="M0,1.71428571 C0,0.76752 0.76752,0 1.71428571,0 L22.2857143,0 C23.2325143,0 24,0.76752 24,1.71428571 C24,2.66105143 23.2325143,3.42857143 22.2857143,3.42857143 L1.71428571,3.42857143 C0.76752,3.42857143 0,2.66105143 0,1.71428571 Z" id="Path"></path>
+                                <path class="color-background" d="M0,10.2857143 C0,9.33894857 0.76752,8.57142857 1.71428571,8.57142857 L22.2857143,8.57142857 C23.2325143,8.57142857 24,9.33894857 24,10.2857143 C24,11.2325143 23.2325143,12 22.2857143,12 L1.71428571,12 C0.76752,12 0,11.2325143 0,10.2857143 Z" id="Path"></path>
+                                <path class="color-background" d="M10.2857143,18.8571429 C10.2857143,17.9103429 11.0532343,17.1428571 12,17.1428571 L22.2857143,17.1428571 C23.2325143,17.1428571 24,17.9103429 24,18.8571429 C24,19.8039429 23.2325143,20.5714286 22.2857143,20.5714286 L12,20.5714286 C11.0532343,20.5714286 10.2857143,19.8039429 10.2857143,18.8571429 Z" id="Path"></path>
+                            </g>
                         </g>
-                    </g>
-                </svg>
-            </div>
-            <span class="nav-link-text ms-1">Logout</span>
-        </a>
-    </li>
-
+                    </svg>
+                </div>
+                <span class="nav-link-text ms-1">Logout</span>
+            </a>
+        </li>
       </ul>
     </div>
     <div class="sidenav-footer mx-4 ">
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use App\Http\Controllers\MedicineController;
 |
 */
 
+// Rute Medicine
+
 Route::redirect('/', '/medicines');
 
 Route::get('/medicines', function () {
@@ -26,3 +28,20 @@ Route::get('/medicines/create', [MedicineController::class, 'create'])->name('me
 Route::get('/medicines/{medicine}/edit', [MedicineController::class, 'edit'])->name('medicines.edit');
 Route::put('/medicines/{medicine}', [MedicineController::class, 'update'])->name('medicines.update');
 Route::delete('/medicines/{medicine}', [MedicineController::class, 'destroy'])->name('medicines.destroy');
+
+// Rute Sementara
+Route::get('/dashboard', function () {
+    return redirect()->route('medicines.index');
+})->name('dashboard');
+
+Route::get('/sales', function () {
+    return redirect()->route('medicines.index');
+})->name('sales.index');
+
+Route::get('/account', function () {
+    return redirect()->route('medicines.index');
+})->name('account.index');
+
+Route::get('/logout', function () {
+    return redirect()->route('medicines.index');
+})->name('logout');
