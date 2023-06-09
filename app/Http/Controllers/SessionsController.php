e4b9973d7934cfde6946ef88aed6b7211aeda114
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-09 18:20:24 +0700

    SessionsController Login(create) Logout (destroy)

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
-    //
+    public function create(): View
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store(): Redirector
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Email salah. Ulangi lagi.',
+                'password' => 'Passwordmu salah. Ulangi lagi.'
+            ]);
+        }
+
+
+        session()->regenerate(); // session fixation
+        return redirect('/')->with('success', 'Welcome Back!');
+    }
+
+    public function destroy(): Redirector
+    {
+        auth()->logout();
+        return redirect('/')->with('success', 'Goodbye!');
+    }
 }
