5df3267328bc43edec9b72b9172031aba628f8f3
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-09 18:29:22 +0700

    Header for Authenticated User and Logout Form Submit

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -60,7 +60,7 @@
         </li>
         <li class="nav-item">
           <a class="nav-link {{ request()->routeIs('accounts.index') ? ' active' : '' }}" href="{{ route('accounts.index') }}">
-            <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">      
+            <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
               <svg width="24" height="24" viewBox="-5 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>account</title>
                 <path d="M24 4c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 6c3.31 0 6 2.69 6 6 0 3.32-2.69 6-6 6s-6-2.68-6-6c0-3.31 2.69-6 6-6zm0 28.4c-5.01 0-9.41-2.56-12-6.44.05-3.97 8.01-6.16 12-6.16s11.94 2.19 12 6.16c-2.59 3.88-6.99 6.44-12 6.44z" fill="#ffffff"/>
@@ -71,7 +71,9 @@
           </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('logout') ? ' active' : ''}}" href="{{ route('logout') }}">
+            <a class="hidden nav-link {{ request()->routeIs('logout') ? ' active' : ''}}"
+               onclick="document.querySelector('#logout-form').submit();"
+               href="#">
                 <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
                     <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                         <title>rtl</title>
@@ -84,6 +86,14 @@
                         </g>
                     </svg>
                 </div>
+
+                <form class="d-none"
+                      id="logout-form"
+                      method="POST"
+                      action="/logout"
+                >@csrf
+                </form>
+
                 <span class="nav-link-text ms-1">Logout</span>
             </a>
         </li>
