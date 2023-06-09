6dc3679d3a73d4e77c3a4e4cea8e8e4b13e5190b
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-09 18:46:18 +0700

    Better Login Page

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -97,9 +97,8 @@
                                 <div
                                     class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
                                     <h2 class="mt-3 text-dark font-weight-bold">Enter our global community of
-                                        developers.</h2>
-                                    <h6 class="text-dark text-sm mt-5">Copyright © 2022 Corporate UI Design System by
-                                        Creative Tim.</h6>
+                                        sleepless undergraduate.</h2>
+                                    <h6 class="text-dark text-sm mt-5">Copyright © 2022 Team 3 of RPL Class A</h6>
                                 </div>
                             </div>
                         </div>
