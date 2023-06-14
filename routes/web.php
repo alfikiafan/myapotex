2d257339c16494e21f6510cf990d699ffcf571c7
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-14 10:12:48 +0700

    Route Update

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,34 @@ Route::post('/login', [SessionsController::class, 'store'])->middleware('guest')
 Route::post('/logout', [SessionsController::class, 'destroy'])->name('logout')->middleware('auth');
 Route::redirect('/home', '/')->middleware('auth');
 
-// Rute Medicine
 
-Route::redirect('/', '/medicines');
+Route::middleware('auth')->group(function () {
+    // Rute Dashboard
+    Route::redirect('/', '/medicines');
 
-Route::get('/medicines', function () {
-    return view('index');
+    // Rute Medicine
+    Route::get('/medicines', [MedicineController::class, 'index'])->name('medicines.index');
+
+    Route::middleware('administrator')->group(function () {
+        Route::post('/medicines', [MedicineController::class, 'store'])->name('medicines.store');
+        Route::get('/medicines/create', [MedicineController::class, 'create'])->name('medicines.create');
+        Route::get('/medicines/{medicine}/edit', [MedicineController::class, 'edit'])->name('medicines.edit');
+        Route::put('/medicines/{medicine}', [MedicineController::class, 'update'])->name('medicines.update');
+        Route::delete('/medicines/{medicine}', [MedicineController::class, 'destroy'])->name('medicines.destroy');
+    });
+
+    // Rute Accounts
+    Route::middleware('administrator')->group(function () {
+        Route::get('/accounts', [UserController::class, 'index'])->name('accounts.index');
+        Route::post('/accounts', [UserController::class, 'store'])->name('accounts.store');
+        Route::get('/accounts/create', [UserController::class, 'create'])->name('accounts.create');
+        Route::get('/accounts/{user}/edit', [UserController::class, 'edit'])->name('accounts.edit');
+        Route::put('/accounts/{user}', [UserController::class, 'update'])->name('accounts.update');
+        Route::delete('/accounts/{user}', [UserController::class, 'destroy'])->name('accounts.destroy');
+    });
+
 });
 
-Route::get('/medicines', [MedicineController::class, 'index'])->name('medicines.index');
-Route::post('/medicines', [MedicineController::class, 'store'])->name('medicines.store');
-Route::get('/medicines/create', [MedicineController::class, 'create'])->name('medicines.create');
-Route::get('/medicines/{medicine}/edit', [MedicineController::class, 'edit'])->name('medicines.edit');
-Route::put('/medicines/{medicine}', [MedicineController::class, 'update'])->name('medicines.update');
-Route::delete('/medicines/{medicine}', [MedicineController::class, 'destroy'])->name('medicines.destroy');
-
-// Rute Accounts
-
-Route::get('/accounts', function () {
-    return view('index');
-})->name('accounts.index');
-
-Route::get('/accounts', [UserController::class, 'index'])->name('accounts.index');
-Route::post('/accounts', [UserController::class, 'store'])->name('accounts.store');
-Route::get('/accounts/create', [UserController::class, 'create'])->name('accounts.create');
-Route::get('/accounts/{user}/edit', [UserController::class, 'edit'])->name('accounts.edit');
-Route::put('/accounts/{user}', [UserController::class, 'update'])->name('accounts.update');
-Route::delete('/accounts/{user}', [UserController::class, 'destroy'])->name('accounts.destroy');
 
 // Rute Sementara
 Route::get('/dashboard', function () {
