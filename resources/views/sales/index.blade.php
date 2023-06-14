edd35a9dcd7ec729e44ff0a30d602c95be9eeeb8
faizfathoni <faizfathoni2002@gmail.com>
2023-06-14 12:02:56 +0700

    Tambah

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -1,37 +1,76 @@
 @extends('layouts.app')
 
-
 @section('content')
-<div class="row">
+<div class="container-fluid py-4 px-5">
+      <div class="row">
         <div class="col-12">
           <div class="card border shadow-xs mb-4">
             <div class="card-header border-bottom pb-0">
               <div class="d-sm-flex align-items-center mb-3">
                 <div>
-                  <h6 class="font-weight-semibold text-lg mb-0">Sale Transaction</h6>
+                  <h6 class="font-weight-semibold text-lg mb-0">Sales Transaction</h6>
                   <p class="text-sm mb-sm-0">Record and process sales transaction</p>
-                 </div>
-
-                 <div class="ms-auto d-flex"> 
-                  <form action="{{ route('medicines.index') }}" method="GET" class="me-3">
-                    <div class="input-group input-group-sm ms-auto">
-                      <span class="input-group-text text-body" type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
-                        </svg>
-                      </span>
-                      <input type="text" class="form-control form-control-sm" name="search" value="{{ request('search') }}" placeholder="Search">
-                    </div> 
-
-                    </form>
+                </div>
+                <div class="ms-auto d-flex">
+                  <div class="me-4">
+                    <h6 class="font-weight-semibold text-m mb-0">ID Transaction: T0001</h6>
+                  </div>
+                  <a href="{{ route('medicines.create') }}">
+                    <button type="button" class="btn btn-sm btn-white btn-icon d-flex align-items-center mb-0 me-2">
+                    <span class="btn-inner--icon me-2">
+                      <i class="fa-solid fa-plus" style="color: #000000;"></i>
+                    </span>
+                      <span class="btn-inner--text">Reset‎ transaction</span>
+                    </button>
+                  </a>
                   <a href="{{ route('medicines.create') }}">
                     <button type="button" class="btn btn-sm btn-dark btn-icon d-flex align-items-center mb-0 me-2">
                     <span class="btn-inner--icon me-2">
                       <i class="fa-solid fa-plus" style="color: #ffffff;"></i>
                     </span>
-                      <span class="btn-inner--text">Add‎ Transaction</span>
+                      <span class="btn-inner--text">Add‎ sales</span>
                     </button>
                   </a>
                 </div>
               </div>
+            </div>
+            <div class="card-body px-0 py-0">
+              <div class="table-responsive p-0">
+              <table class="table align-items-center mb-0">
+                  <thead class="bg-gray-100">
+                    <tr>
+                      <th class="text-secondary text-xs font-weight-semibold">No.</th>
+                      <th class="text-secondary text-xs font-weight-semibold">ID</th>
+                      <th class="text-secondary text-xs font-weight-semibold">Medicine Name</th>
+                      <th class="text-secondary text-xs font-weight-semibold">Quantity</th>
+                      <th class="text-secondary text-xs font-weight-semibold">Price</th>
+                      <th class="text-secondary text-xs font-weight-semibold">Discount</th>
+                      <th class="text-secondary text-xs font-weight-semibold">Subtotal</th>
+                      <th class="text-secondary text-xs font-weight-semibold">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td class="ps-4">
+                      <div class="d-flex align-items-center">
+                        <a href="">
+                          <button type="button" class="btn btn-sm btn-primary mb-0 me-1">
+                            <i class="fas fa-pencil-alt"></i>
+                          </button>
+                        </a>
+                        <form action="" method="POST">
+                            <button type="submit" class="btn btn-sm mb-0 ms-1 btn-danger" onclick="return confirm('Are you sure to delete this medicine?')">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </form>
+                      </div>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     @endsection
