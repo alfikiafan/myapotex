8c37f66d4791168216821a7ef5cd9a263c598a2c
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-17 13:17:48 +0700

    Fix Medicine by search to work with other orderBy
    - quantity
    - discount
    - price
    - search

--- resources/views/medicines/index.blade.php
+++ resources/views/medicines/index.blade.php
@@ -31,12 +31,15 @@ function sortDir($direction){
                 <div class="ms-auto d-flex">
                   <form action="{{ route('medicines.index') }}" method="GET" class="me-3">
                     <div class="input-group input-group-sm ms-auto">
-                      <span class="input-group-text text-body" type="submit">
+                      <button class="input-group-text text-body" type="submit">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
                         </svg>
-                      </span>
-                      <input type="text" class="form-control form-control-sm" name="search" value="{{ request('search') }}" placeholder="Search">
+                      </button>
+                        @foreach(request()->except('search') as $key => $value)
+                            <input type="hidden" name="{{ $key }}" value="{{ $value }}"/>
+                        @endforeach
+                      <input type="text" class="form-control form-control-sm" name="search" value="{{ request('search') }}" placeholder="Search"/>
                     </div>
                   </form>
                     @can('admin')
