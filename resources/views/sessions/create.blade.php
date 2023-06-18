7dc24106f544fbdf8add491d2af18bc366e35af9
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-06-18 11:39:10 +0700

    Slight changes in login and dashboard page

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,101 +1,84 @@
 @include('partials.head')
 
 <body>
-<div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-        <div class="col-12">
-            <!-- Navbar -->
-            <nav
-                class="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-                <div class="container-fluid">
-                    <x-svg name="logo" :width="300" :height="50"/>
-
-                </div>
-            </nav>
-            <!-- End Navbar -->
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                <!-- Navbar -->
+                <nav
+                    class="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+                    <div class="container-fluid">
+                        <x-svg name="logo" :width="300" :height="50"/>
+                    </div>
+                </nav>
+                <!-- End Navbar -->
+            </div>
         </div>
     </div>
-</div>
-<main class="main-content  mt-0">
-    <section>
-        <div class="page-header min-vh-100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4 col-md-6 d-flex flex-column mx-auto">
-                        <div class="card card-plain mt-8">
-                            <div class="card-header pb-0 text-left bg-transparent">
-                                <h3 class="font-weight-black text-dark display-6">Welcome back</h3>
-                                <p class="mb-0">Welcome back! Please enter your details.</p>
-                            </div>
-                            <div class="card-body">
-                                <form role="form" method="POST" action="/login">
-                                    @csrf
-                                    <label>Email</label>
-                                    <div class="mb-3">
-                                        <input type="email" name="email" id="email" class="form-control"
-                                               placeholder="Enter your email address"
-                                               aria-label="Email" aria-describedby="email-addon">
-                                    </div>
-                                    <label>Password</label>
-                                    <div class="mb-3">
-                                        <input type="password" name="password" id="password" class="form-control"
-                                               placeholder="Enter password"
-                                               aria-label="Password" aria-describedby="password-addon">
-                                    </div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="form-check form-check-info text-left mb-0">
-                                            <input class="form-check-input" type="checkbox" value=""
-                                                   id="flexCheckDefault">
-                                            <label class="font-weight-normal text-dark mb-0" for="flexCheckDefault">
-                                                Remember for 14 days
-                                            </label>
+    <main class="main-content  mt-0">
+        <section>
+            <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-md-6 d-flex flex-column mx-auto">
+                            <div class="card card-plain mt-8">
+                                <div class="card-header pb-0 text-left bg-transparent">
+                                    <h3 class="font-weight-black text-dark display-6">Welcome back</h3>
+                                    <p class="mb-0">Welcome back! Please enter your details.</p>
+                                </div>
+                                <div class="card-body">
+                                    <form role="form" method="POST" action="/login">
+                                        @csrf
+                                        <label>Email</label>
+                                        <div class="mb-3">
+                                            <input type="email" name="email" id="email" class="form-control"
+                                                placeholder="Enter your email address"
+                                                aria-label="Email" aria-describedby="email-addon">
                                         </div>
-                                        <a href="javascript:;" class="text-xs font-weight-bold ms-auto">Forgot
-                                            password</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Sign in</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                                <p class="mb-4 text-xs mx-auto">
-                                    Don't have an account?
-                                    <a href="javascript:;" class="text-dark font-weight-bold">Sign up</a>
-                                </p>
+                                        <label>Password</label>
+                                        <div class="mb-3">
+                                            <input type="password" name="password" id="password" class="form-control"
+                                                placeholder="Enter password"
+                                                aria-label="Password" aria-describedby="password-addon">
+                                        </div>
+                                        <div class="d-flex align-items-center">
+                                            <div class="form-check form-check-info text-left mb-0">
+                                                <input class="form-check-input" type="checkbox" value=""
+                                                    id="flexCheckDefault">
+                                                <label class="font-weight-normal text-dark mb-0" for="flexCheckDefault">
+                                                    Remember for 14 days
+                                                </label>
+                                            </div>
+                                            <a href="javascript:;" class="text-xs font-weight-bold ms-auto">Forgot
+                                                password</a>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Sign in</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                    <p class="mb-4 text-xs mx-auto">Retrieve your account by contacting the administrator.</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="position-absolute w-40 top-0 end-0 h-100 d-md-block d-none">
-                            <div
-                                class="oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 bg-cover ms-n8"
-                                style="background-image:url('/img/image-sign-in.jpg')">
+                        <div class="col-md-6">
+                            <div class="position-absolute w-40 top-0 end-0 h-100 d-md-block d-none">
                                 <div
-                                    class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
-                                    <h2 class="mt-3 text-dark font-weight-bold">Enter our global community of
-                                        sleepless undergraduate.</h2>
-                                    <h6 class="text-dark text-sm mt-5">Copyright © 2022 Team 3 of RPL Class A</h6>
+                                    class="oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 bg-cover ms-n8"
+                                    style="background-image:url('/img/image-sign-in.jpg')">
+                                    <div
+                                        class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
+                                        <h2 class="mt-3 text-dark font-weight-bold">Empowering healthier futures, together.</h2>
+                                        <h6 class="text-dark text-sm mt-5">Copyright © 2023 Team 3 of RPL Class A</h6>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</main>
-
-<!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="../assets/js/corporate-ui-dashboard.min.js?v=1.0.0"></script>
-<script src="{{ asset('js/core/popper.min.js') }}"></script>
-<script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
-<!-- Github buttons -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="{{ asset('js/corporate-ui-dashboard.js') }}?v=1.0.0"></script>
+        </section>
+    </main>
 </body>
-
 </html>
